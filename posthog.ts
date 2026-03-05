6057c44d016a1b8a9f5c8acdc9562dc76df84fd9
SatyamVyas04 <vyas.satyam.04@gmail.com>
2026-03-06 00:27:17 +0530

    fix: update PostHog API and UI host URLs in initialization

--- posthog.ts
+++ posthog.ts
@@ -2,6 +2,7 @@ import posthog from "posthog-js";
 
 // biome-ignore lint/style/noNonNullAssertion: PostHog key is required for instrumentation to work
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  api_host: "https://p.sensory-ui.com",
+  ui_host: "https://us.posthog.com",
   defaults: "2026-01-30",
 });
